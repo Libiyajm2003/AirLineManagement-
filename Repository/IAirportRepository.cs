12d83bab97228e1dac1104e74c202f097507538e
Libiyajm2003 <libiyajm2704@gmail.com>
2025-09-22 15:19:33 +0530

    Update IAirportRepository.cs

--- Repository/IAirportRepository.cs
+++ Repository/IAirportRepository.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 
 namespace ConsoleAppAirLineManagement.Repository
 {
+//Interface for Airport repository to handle data access related to airports
     public interface IAirportRepository
     {
+        //Retrieves all airports from the database
         List<Airport> GetAllAirports();
-        Airport GetAirportById(int airportId);
-        void AddAirport(Airport airport);
-        void UpdateAirport(Airport airport);
-        void DeleteAirport(int airportId);
+        Airport GetAirportById(int airportId); //get by id
+        void AddAirport(Airport airport);// add the airport
+        void UpdateAirport(Airport airport); //update the airport
+        void DeleteAirport(int airportId); // delete the airport
     }
 }
