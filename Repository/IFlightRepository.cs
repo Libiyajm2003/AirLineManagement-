eada05a7010e59c2ab490acfe688ec6c07b01502
Libiyajm2003 <libiyajm2704@gmail.com>
2025-09-22 15:21:05 +0530

    Update IFlightRepository.cs

--- Repository/IFlightRepository.cs
+++ Repository/IFlightRepository.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 
 namespace ConsoleAppAirLineManagement.Repository
 {
+//Interface for Flight repository to handle data access related to flights
     public interface IFlightRepository
     {
+        //Retrieves all flights from the database
         List<Flight> GetAllFlights();
-        Flight GetFlightById(int flightId);
+        Flight GetFlightById(int flightId); //get flight by id
 
         // Change return type to int
-        int AddFlight(Flight flight);
+        int AddFlight(Flight flight); //add flight
 
-        void UpdateFlight(Flight flight);
-        void DeleteFlight(int flightId);
+        void UpdateFlight(Flight flight); // update flight
+        void DeleteFlight(int flightId);// delete flight
 
         List<Airport> GetAllAirports();
     }
