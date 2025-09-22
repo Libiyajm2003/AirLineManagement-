2e35ebe3f13a29faf7fee103f8b88823f9d8646d
Libiyajm2003 <libiyajm2704@gmail.com>
2025-09-22 15:09:20 +0530

    Update AdminRepositoryImpl.cs

--- Repository/AdminRepositoryImpl.cs
+++ Repository/AdminRepositoryImpl.cs
@@ -3,14 +3,17 @@ using Microsoft.Data.SqlClient;
 
 namespace ConsoleAppAirLineManagement.Repository
 {
+//Implementation of IAdminRepository for Admin login validation
     public class AdminRepositoryImpl : IAdminRepository
     {
+    //Connection string to the SQL Server database
         private string connStr = @"Data Source=localhost;Initial Catalog=FlyWithMeDB;Integrated Security=True;TrustServerCertificate=True";
 
         public bool ValidateLogin(string username, string password)
         {
             using (SqlConnection con = new SqlConnection(connStr))
             {
+            //Validates the admin login using stored procedure sp_ValidateAdminLogin
                 SqlCommand cmd = new SqlCommand("sp_ValidateAdminLogin", con);
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
