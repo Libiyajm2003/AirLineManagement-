bfedda153a1c1fa5dd127e0a07f7f0fb2a31cfd4
Libiyajm2003 <libiyajm2704@gmail.com>
2025-09-22 14:37:40 +0530

    Update Admin.cs

--- Model/Admin.cs
+++ Model/Admin.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppAirLineManagement.Model
 {
-
+// create admin class 
     public class Admin
     {
+        //fields / properties [used for login]
         public int AdminId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
