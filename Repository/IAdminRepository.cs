4abc56f3b962dd10ddffbfdd21e0c2483bf7f5bd
Libiyajm2003 <libiyajm2704@gmail.com>
2025-09-22 15:17:44 +0530

    Update IAdminRepository.cs

--- Repository/IAdminRepository.cs
+++ Repository/IAdminRepository.cs
@@ -2,8 +2,10 @@
 
 namespace ConsoleAppAirLineManagement.Repository
 {
+//Interface for Admin repository to handle data access related to Admin
     public interface IAdminRepository
     {
+        //Validates the admin login credentials
         bool ValidateLogin(string username, string password);
     }
 }
