70ab9f0150b3c281a7e0b6e058b541a33ad0be03
Libiyajm2003 <libiyajm2704@gmail.com>
2025-09-22 14:46:14 +0530

    Update Airport.cs

--- Model/Airport.cs
+++ Model/Airport.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppAirLineManagement.Model
 {
+//create airport class
         public class Airport
         {
+                //fields /properties
             public int AirportId { get; set; }
             public string AirportCode { get; set; }
             public string AirportName { get; set; }
