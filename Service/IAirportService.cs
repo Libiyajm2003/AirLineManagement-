753bcc31161528340d4bead323df751cff9ca4fc
Libiyajm2003 <libiyajm2704@gmail.com>
2025-09-22 15:01:46 +0530

    Update IAirportService.cs

--- Service/IAirportService.cs
+++ Service/IAirportService.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 
 namespace ConsoleAppAirLineManagement.Service
 {
+//create interface of IAirportService
     public interface IAirportService
     {
-        List<Airport> GetAllAirports();
-        Airport GetAirportById(int airportId);
-        void AddAirport(Airport airport);
-        void UpdateAirport(Airport airport);
-        void DeleteAirport(int airportId);
+        List<Airport> GetAllAirports(); //get all airports
+        Airport GetAirportById(int airportId);// get aiports by id
+        void AddAirport(Airport airport); //add airports
+        void UpdateAirport(Airport airport);// update airports
+        void DeleteAirport(int airportId); //delete airports
     }
 }
 
